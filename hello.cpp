ca406f053fb1c870167a26037ecef2628ea4d170
Devanand Yadav <63476064+devanand369@users.noreply.github.com>
2021-06-26 13:48:14 +0530

    create a hello.cpp file
    This program only print hello world .

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+/* This is a simple Hello world program in C++ for demo*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  cout<<"Hello World ! Welcome to coding on github Codespace ."<<endln;
+  
+  return 0;
+}
